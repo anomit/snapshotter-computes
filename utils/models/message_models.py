7ce3132f2fcae0e4351301161a1478f35bf3d0b5
Anomit Ghosh <anomit.ghosh@gmail.com>
2024-04-04 14:22:41 +0530

    Update message_models.py to include tx event logs

--- utils/models/message_models.py
+++ utils/models/message_models.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
+from typing import List
+from snapshotter.utils.models.message_models import TxLogsModel
 
 class TrackingWalletInteractionSnapshot(BaseModel):
     wallet_address: str
     contract_address: str
+    logs: List[TxLogsModel]
